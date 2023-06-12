a56cab57d8edb53e0c0840d4e9cdf45252a8fdb9
Twan Kuipers <twankuipers@home.nl>
2023-06-12 18:24:45 +0200

    expanded rendrobjectlayer

--- editor/js/renderer.js
+++ editor/js/renderer.js
@@ -59,12 +59,43 @@ function renderTileLayer() {
 function renderObjectLayer(layer) {
     const objects = layer.objects;
     objects.forEach(obj => {
+        // if (obj.gid) {
+        //     switch (obj.gid){
+        //         case 24: //button
+        //             break;
+        //         case 25: //lever
+        //             break;
+        //         case 26: //lever
+        //             break;
+        //         case 28: //box
+        //             break;
+        //         case 29: //licht emitter
+        //             break;
+        //         case 30: //rotation boxmirror
+        //             break;
+        //         case 31: //light receiver
+        //             break;
+        //         case 34: //ball
+        //             break;
+        //         case 35: //rotation mirror
+        //             break;
+        //         case 36: //boxmirror
+        //             break;
+        //         case 37: //heavy box
+        //             break;
+        //         case 38: //timed button
+        //             break;
+        //         case 39: //wind generator
+        //             break;
+        //     }
+        // } else if (obj.type == 'platform') { //platform
+        // }
         objCtx.beginPath();
         let objY = obj.y;
         if (obj.type === 'platform') {
             objY += obj.height;
         };
-        objCtx.rect(obj.x, obj.y, obj.width, -obj.height)
+        objCtx.rect(obj.x, obj.y, obj.width, obj.height)
         objCtx.fillStyle = 'purple';
         objCtx.fill();
     });
