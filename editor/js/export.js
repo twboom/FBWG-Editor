d82078af0578971f3409a34765d0b960eda53f64
twboom <twboom@hotmail.nl>
2023-06-15 22:00:05 +0200

    Added export to clip button

--- editor/js/export.js
+++ editor/js/export.js
@@ -1,19 +1,22 @@
+function createJSON(LevelTemplate, callback) {
+    LevelTemplate.height = LEVEL.HEIGHT;
+    LevelTemplate.width = LEVEL.WIDTH;
+    LevelTemplate.layers[0].data = LEVEL.TILELAYER;
+    LevelTemplate.layers[0].width = LEVEL.WIDTH;
+    LevelTemplate.layers[0].height = LEVEL.HEIGHT;
+    LevelTemplate.layers[2] = LEVEL.CHARSLAYER;
+    LevelTemplate.layers[1] = LEVEL.OBJECTLAYER;
+
+    // downloadObjectAsJson(LevelTemplate, 'level');
+    return callback(LevelTemplate)
+};
+
 function exportLevel() {
-    function createJSON(LevelTemplate) {
-        LevelTemplate.height = LEVEL.HEIGHT;
-        LevelTemplate.width = LEVEL.WIDTH;
-        LevelTemplate.layers[0].data = LEVEL.TILELAYER;
-        LevelTemplate.layers[0].width = LEVEL.WIDTH;
-        LevelTemplate.layers[0].height = LEVEL.HEIGHT;
-        LevelTemplate.layers[2] = LEVEL.CHARSLAYER;
-        LevelTemplate.layers[1] = LEVEL.OBJECTLAYER;
-
-        downloadObjectAsJson(LevelTemplate, 'level');
-    };
-
     fetch('blank_level.json')
         .then(r => r.json())
-        .then( LevelTemplate => createJSON(LevelTemplate))
+        .then( LevelTemplate => createJSON(LevelTemplate, level => {
+            downloadObjectAsJson(level, 'level')
+        }))
 };
 
 
@@ -27,8 +30,24 @@ function downloadObjectAsJson(exportObj, exportName){
     downloadAnchorNode.remove();
 }
 
+function exportAsText() {
+    fetch('blank_level.json')
+        .then(r => r.json())
+        .then( LevelTemplate => createJSON(LevelTemplate, level => {
+            var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(level));
+            navigator.clipboard.writeText(JSON.stringify(dataStr).replaceAll('"', '')).then(() => {
+                alert('Level content copied to clipboard');
+                /* Resolved - text copied to clipboard successfully */
+              },() => {
+                alert('Failed to copy');
+                /* Rejected - text failed to copy to the clipboard */
+              }); 
+        }))
+}
+
 function initExport() {
     document.getElementById('export').addEventListener('click', exportLevel);
+    document.getElementById('export-url').addEventListener('click', exportAsText);
 }
 
 initExport();
