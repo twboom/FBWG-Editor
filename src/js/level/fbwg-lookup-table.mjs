d99f2da6f7c90bf5fb320016c502be56214ed54a
Thijs Boom <twboom@hotmail.nl>
2026-05-07 20:44:40 +0200

    Added mech group lookup table

--- src/js/level/fbwg-lookup-table.mjs
+++ src/js/level/fbwg-lookup-table.mjs
@@ -10,3 +10,14 @@ export const TILE_LOOKUP = {
     8: 'acid',
     15: 'ice',
 }
+
+export const GROUP_LOOKUP = {
+    1: 'red',
+    2: 'green',
+    3: 'blue',
+    4: 'yellow',
+    5: 'magenta',
+    6: 'cyan',
+    7: 'violet',
+    8: 'white',
+};
--- src/js/level/fbwg-lookup.mjs
+++ src/js/level/fbwg-lookup.mjs
@@ -1,4 +1,4 @@
-import { TILE_LOOKUP } from "./fbwg-lookup-table.mjs";
+import { GROUP_LOOKUP, TILE_LOOKUP } from "./fbwg-lookup-table.mjs";
 
 function getKey(obj, value) {
     return Object.keys(obj).find(key => obj[key] === value);
@@ -20,4 +20,22 @@ export function tileLookup(tileInternal) {
  */
 export function reverseTileLookup(tileExternal) {
     return TILE_LOOKUP[tileExternal];
+};
+
+/**
+ * Look up the external reference based on the internal reference.
+ * @param {string} groupInternal 
+ * @returns {number}
+ */
+export function groupLookup(groupInternal) {
+    return getKey(GROUP_LOOKUP, groupInternal);
+};
+
+/**
+ * Look up the internal reference based on the external reference.
+ * @param {number} groupExternal 
+ * @returns {string}
+ */
+export function reverseGroupLookup(groupExternal) {
+    return GROUP_LOOKUP[groupExternal];
 };
